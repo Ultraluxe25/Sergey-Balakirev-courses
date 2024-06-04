5799077fecb430b40695c5d88ebad6ae71259bad
Alexander Kapturov <ultraluxe25@mail.ru>
2024-06-04 15:25:26 +0300

    Completed 12th lesson of Balakirevs C and 4.2-4.3 of Young C coder

---
+++ C, C++ course/Course programs/lecture_12.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+    int v1, v2, v3, v4, v5, v6;
+    v1 = -10 / 4;
+    v2 = -10 / -4;
+    v3 = 10 / -4;
+    v4 = -10 % 4;
+    v5 = -10 % -4;
+    v6 = 10 % 4;
+
+    return 0;
+}
